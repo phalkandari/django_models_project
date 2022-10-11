45b7801f1e40b74d864c87c603f52cf50aa0f87e
phalkandari <mohammad.kandary@gmail.com>
2022-10-11 17:16:06 +0300

    commiting after creating the restaurant class

---
+++ restaurants/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from email.policy import default
+from unicodedata import name
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
